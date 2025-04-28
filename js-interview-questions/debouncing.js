5743b16d3673b96bd25cc869bbca049dc784c34f
Balkishan <bkbajpay0609@gmail.com>
2025-04-28 13:37:08 +0530

    updates

--- js-interview-questions/debouncing.js
+++ js-interview-questions/debouncing.js
@@ -1,4 +1,9 @@
 //"Wait until the user stops doing something for a certain time, and then run the function once."
+
+// Debounce → "I’ll wait till you stop talking, then I’ll answer."
+
+// Throttle → "I’ll answer once every 1 second, no matter how much you talk."
+
 let counter = 0;
 function getData(){
     console.log("fetching Data"+counter++);
